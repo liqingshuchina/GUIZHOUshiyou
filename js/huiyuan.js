baf4685e364912e614f82024b38f4a4017a08bc6
李青书 <1528455060@qq.com>
2019-05-03 16:11:38 +0800

    xiugai//--2/3

--- js/huiyuan.js
+++ js/huiyuan.js
@@ -4,6 +4,7 @@ window.onload=function(){
 	vbaocun.onclick=function(){
 		this.classList.add("mui-action-back");
 		atip("修改成功");
+		this.innerHTML="返回";
 	}
 	function atip(info){
 		mui.toast(info)
